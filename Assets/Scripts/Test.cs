8b7570d5633d92f6b28f784292ad8cd5feceb2ae
Siguma <you8jun5@gmail.com>
2018-03-18 16:20:31 +0900

    サンプルで出力されたgRPCの通信はうまくいった

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Grpc.Core;
+using Helloworld;
+
+public class Test : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Say();
+        }
+    }
+
+    private void Say() {
+        Debug.Log("hello world");
+
+        Channel channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+        var client = new Greeter.GreeterClient(channel);
+
+        string user = "you";
+
+        var reply = client.SayHello(new HelloRequest { Name = user });
+        Debug.Log("Greeting: " + reply.Message);
+
+        channel.ShutdownAsync().Wait();
+    }
+
+}
